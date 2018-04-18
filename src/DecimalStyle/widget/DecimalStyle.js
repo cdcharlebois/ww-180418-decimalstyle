c977df64de81c5385555208a9d3e25c11c8f821b
Conner Charlebois <cdcharlebois@gmail.com>
2018-04-18 16:56:21 +1000

    init

---
+++ src/DecimalStyle/widget/DecimalStyle.js
@@ -0,0 +1,94 @@
+define([
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "mxui/dom",
+    "dojo/dom",
+    "dojo/dom-prop",
+    "dojo/dom-geometry",
+    "dojo/dom-class",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/text",
+    "dojo/html",
+    "dojo/_base/event",
+
+    "dojo/text!DecimalStyle/widget/template/DecimalStyle.html"
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, widgetTemplate) {
+    "use strict";
+
+    return declare("DecimalStyle.widget.DecimalStyle", [_WidgetBase, _TemplatedMixin], {
+
+        templateString: widgetTemplate,
+
+        widgetBase: null,
+
+        // html/template nodes
+        beforeNode: null,
+        decimalNode: null,
+        afterNode: null,
+
+        // modeler properties
+        field: null,
+        beforeClassname: null,
+        afterClassname: null,
+
+        // Internal variables.
+        _handles: null,
+        _contextObj: null,
+
+        constructor: function() {
+            this._handles = [];
+        },
+
+        postCreate: function() {
+            logger.debug(this.id + ".postCreate");
+            // Set the classname of our nodes
+        },
+
+        update: function(obj, callback) {
+            console.log(obj);
+            // two responsibilities
+            // 1. set the context object
+            logger.debug(this.id + ".update");
+            this._contextObj = obj;
+            // Get the value from the field
+            var value = this._contextObj.get(this.field); // decimal
+            // Split into left and right parts
+            // Set the innerHTML of the before and after nodes
+
+            // 2. Call the callback 
+            this._updateRendering(callback);
+        },
+
+        resize: function(box) {
+            logger.debug(this.id + ".resize");
+        },
+
+        uninitialize: function() {
+            logger.debug(this.id + ".uninitialize");
+        },
+
+        _updateRendering: function(callback) {
+            logger.debug(this.id + "._updateRendering");
+
+            if (this._contextObj !== null) {
+                dojoStyle.set(this.domNode, "display", "block");
+            } else {
+                dojoStyle.set(this.domNode, "display", "none");
+            }
+
+            this._executeCallback(callback);
+        },
+
+        _executeCallback: function(cb) {
+            if (cb && typeof cb === "function") {
+                cb();
+            }
+        }
+    });
+});
+
+require(["DecimalStyle/widget/DecimalStyle"]);
